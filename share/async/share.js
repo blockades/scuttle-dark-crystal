e2bf4f5c36e939ee4529e13e1e8425c16676cb9d
Kieran <kieran.a.gibb@gmail.com>
2018-07-17 17:25:58 +0100

    search for rituals attached to root before publishing

--- share/async/share.js
+++ share/async/share.js
@@ -1,33 +1,72 @@
 const secrets = require('secrets.js-grempe')
 
-const GetRoot = require('./get/root')
-const PublishRoot = require('./publish/root')
-const PublishRitual = require('./publish/ritual')
-const PublishShard = require('./publish/shard')
+const GetRoot = require('../../root/async/get')
+const PublishRoot = require('../../root/async/publish')
+const PublishRitual = require('../../ritual/async/publish')
+const PublishShard = require('../../shard/async/publish')
+
+const { isFeed } = require('ssb-ref')
 
 module.exports = function (server) {
   const getRoot = GetRoot(server)
   const publishRitual = PublishRitual(server)
 
   return function ({ name, rootId, secret, quorum, recps }, callback) {
+    recps = sanitize(recps)
+    let includesSelf = recps.includes(server.id)
+
+    if (recps.length < 1) return callback(new Error(`recps: must be a feedId`))
+    if (includesSelf) return callback(new Error(`recps: doesn't need to include self`))
+
     const numOfShards = recps.length
+    if (Number(numOfShards) < Number(quorum)) {
+      var error = new Error(`data.quorum: greater than number of recps`)
+      return callback(error)
+    }
+
     const shards = secrets.share(secret, numOfShards, quorum)
 
     getRoot(rootId, (err, root) => {
       if (!root) return publishRoot({ name }, performSecretRitual)
-      else return performSecretRitual(err, root)
+      else {
+        const opts = {
+          live: false,
+          reverse: false,
+          query: [{
+            $filter: {
+              value: {
+                timestamp: { gt: 0 },
+                author: server.id,
+                content: {
+                  type: `dark-crystal/ritual`,
+                  root: root.key
+                }
+              }
+            }
+          }]
+        }
+
+        pull(
+          server.query.read(opts),
+          pull.collect((err, msgs) => {
+            if (err) return callback(err)
+            if (msgs.length > 1) return callback(new Error('data.root: already has ritual'))
+            performSecretRitual(err, root)
+          })
+        )
+      }
     })
 
-    function performSecretRitual (err, root) => {
+    function performSecretRitual (err, root) {
       if (err) return callback(err)
 
-      publishRitual({ root, shards: numOfShards, quorum }, (err, ritual) => {
+      publishRitual({ root: root.key, shards: numOfShards, quorum }, (err, ritual) => {
         if (err) return callback(err)
 
         var params = Array.from(Array(numOfShards).keys()).reduce((acc, index) => {
           acc.push({
             root: root.key,
-            shard: shards[index],
+            shard: box(shard, [recp]),
             recps: [ recps[index], server.id ]
           })
           return acc
@@ -42,6 +81,15 @@ module.exports = function (server) {
           })
         )
       })
+    }
+
+    function sanitizeRecps (recps) {
+      let feedIds = recps
+        .map(recp => typeof recp === 'string' ? recp : recp.link)
+        .filter(Boolean)
+        .filter(isFeed)
+
+      return [...new Set(feedIds)]
     }
   }
 }
