f9ea11b77b1cff36505aa289900499a43beb16f2
ameba23 <ameba23@systemli.org>
2018-07-17 19:50:46 +0200

    remove recps from publish shard method

--- shard/async/publish.js
+++ shard/async/publish.js
@@ -5,8 +5,7 @@ module.exports = function (server) {
   return function publish (params, callback) {
     var content = Object.assign({}, {
       type: 'dark-crystal/shard',
-      version: SCHEMA_VERSION,
-      recps: [server.id]
+      version: SCHEMA_VERSION
     }, params)
 
     if (isShard(content)) server.publish(content, callback)
