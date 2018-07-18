30c7e800c396ed4cf3be4553f683edf96bbcce22
Kieran <kieran.a.gibb@gmail.com>
2018-07-18 12:09:12 +0100

    refactor with error handling, validate all shards before publishing any

--- share/async/share.js
+++ share/async/share.js
@@ -1,95 +1,128 @@
 const secrets = require('secrets.js-grempe')
 
-const GetRoot = require('../../root/async/get')
 const PublishRoot = require('../../root/async/publish')
 const PublishRitual = require('../../ritual/async/publish')
-const PublishShard = require('../../shard/async/publish')
+const PublishAllShards = require('../../shard/async/publishAll')
 
 const { isFeed } = require('ssb-ref')
 
 module.exports = function (server) {
-  const getRoot = GetRoot(server)
+  const publishRoot = PublishRoot(server)
   const publishRitual = PublishRitual(server)
+  const publishAllShards = PublishAllShards(server)
 
-  return function ({ name, rootId, secret, quorum, recps }, callback) {
-    recps = sanitize(recps)
-    let includesSelf = recps.includes(server.id)
-
-    if (recps.length < 1) return callback(new Error(`recps: must be a feedId`))
-    if (includesSelf) return callback(new Error(`recps: doesn't need to include self`))
-
-    const numOfShards = recps.length
-    if (Number(numOfShards) < Number(quorum)) {
-      var error = new Error(`data.quorum: greater than number of recps`)
-      return callback(error)
+  return function ({ name, secret, quorum, recps }, callback) {
+    if (!callback && typeof callback !== 'function') {
+      throw new Error('provide a callback')
     }
 
-    const shards = secrets.share(secret, numOfShards, quorum)
+    recps = sanitize(recps)
 
-    getRoot(rootId, (err, root) => {
-      if (!root) return publishRoot({ name }, performSecretRitual)
-      else {
-        const opts = {
-          live: false,
-          reverse: false,
-          query: [{
-            $filter: {
-              value: {
-                timestamp: { gt: 0 },
-                author: server.id,
-                content: {
-                  type: `dark-crystal/ritual`,
-                  root: root.key
-                }
-              }
-            }
-          }]
-        }
+    var validator = prePublishValidation({ name, quorum, recps })
+    if (!validator.valid) return callback(valid.errors.join(', '))
 
-        pull(
-          server.query.read(opts),
-          pull.collect((err, msgs) => {
-            if (err) return callback(err)
-            if (msgs.length > 1) return callback(new Error('data.root: already has ritual'))
-            performSecretRitual(err, root)
-          })
-        )
-      }
-    })
+    const numOfShards = recps.length
 
-    function performSecretRitual (err, root) {
+    // IDEA: Could be cool to allow for object storage?
+    // Permit upload of JSON, YML, etc, files...
+    const hexSecret = secrets.str2hex(secret)
+    const shards = secrets.share(hexSecret, numOfShards, quorum)
+
+    publishRoot({ name }, (err, root) => {
       if (err) return callback(err)
 
+      // Should specify this shards field ACTUALLY as a number - more descriptive
       publishRitual({ root: root.key, shards: numOfShards, quorum }, (err, ritual) => {
         if (err) return callback(err)
 
-        var params = Array.from(Array(numOfShards).keys()).reduce((acc, index) => {
-          acc.push({
-            root: root.key,
-            shard: box(shard, [recp]),
-            recps: [ recps[index], server.id ]
-          })
-          return acc
-        }, [])
-
-        pull(
-          pull.values(params),
-          pull.asyncMap(publishShard),
-          pull.collect((err, shards) => {
-            if (err) return callback(err)
-            callback(null, msgs)
-          })
-        )
+        // QUESTION: Can we somehow wrap the shard publication in a single database 'transaction'?
+        // SOLUTION: Have a publishShards (plural) function which validates each with isShard before publishing all
+        // RESOLUTION: Extracted reducer into a publishAll function
+        publishShards({ shards, recps, rootId }, (err, shards) => {
+          if (err) callback(err)
+          else callback(null, shards)
+        })
       })
-    }
+    })
 
-    function sanitizeRecps (recps) {
-      let feedIds = recps
-        .map(recp => typeof recp === 'string' ? recp : recp.link)
-        .filter(Boolean)
-        .filter(isFeed)
+    function prePublishValidation ({ recps,  }) {
+      var errors = []
+      if (recps.length < 1)
+        errors.push(new Error(`data.recps: must be a feedId`))
 
-      return [...new Set(feedIds)]
+      if (recps.includes(servereid))
+        errors.push(new Error(`data.recps: doesn't need to include self`))
+
+      if (!isNumber(quorum))
+        errors.push(new Error('data.quorum: must be a number'))
+
+      if (quorum === 0)
+        errors.push(new Error(`data.quorum: must be greater than 0`))
+
+      if (recps.length < quorum)
+        errors.push(new Error(`data.quorum: greater than number of recps`))
+
+      return {
+        valid: errors.length === 0,
+        errors: errors
+      }
     }
   }
 }
+
+function isNumber (variable) {
+  return Number(variable) !== NaN
+}
+
+function sanitizeRecps (recps) {
+  if (!Array.isArray(recps)) return []
+
+  // this removes the { name: 'thingy', link: '@...' },
+  // instead just returns feedId '@...'
+  let feedIds = recps
+    .map(recp => typeof recp === 'string' ? recp : recp.link)
+    .filter(Boolean)
+    .filter(isFeed)
+
+  return [...new Set(feedIds)]
+}
+
+// getRoot(rootId, (err, root) => {
+//   if (!root) return publishRoot({ name }, performSecretRitual)
+//   else {
+//     pull(
+//       myRituals()
+//       pull.collect((err, msgs) => {
+//         if (err) return callback(err)
+//         if (msgs.length > 1) return callback(new Error('data.root: already has ritual'))
+//         performSecretRitual(err, root)
+//       })
+//     )
+//   }
+// })
+
+// function performSecretRitual (err, root) {
+//   if (err) return callback(err)
+
+//   publishRitual({ root: root.key, shards: numOfShards, quorum }, (err, ritual) => {
+//     if (err) return callback(err)
+
+//     var params = Array.from(Array(numOfShards).keys()).reduce((acc, index) => {
+//       acc.push({
+//         root: root.key,
+//         shard: box(shard, [recp[index]]),
+//         recipient: recps[index]
+//       })
+//       return acc
+//     }, [])
+
+//     pull(
+//       pull.values(params),
+//       pull.asyncMap(publishShard),
+//       pull.collect((err, shards) => {
+//         if (err) return callback(err)
+//         callback(null, msgs)
+//       })
+//     )
+//   })
+// }
