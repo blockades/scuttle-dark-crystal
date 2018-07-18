ca6a7af8fb451e9ca3cf7b5bb925c7fe38bac516
Kieran <kieran.a.gibb@gmail.com>
2018-07-18 16:34:02 +0100

    begin testing share.js

--- share/async/share.js
+++ share/async/share.js
@@ -6,18 +6,32 @@ const PublishAllShards = require('../../shard/async/publishAll')
 
 const { isFeed } = require('ssb-ref')
 
+const isNumber = require('../../lib/isNumber')
+const isString = require('../../lib/isString')
+const isFunction = require('../../lib/isFunction')
+
 module.exports = function (server) {
   const publishRoot = PublishRoot(server)
   const publishRitual = PublishRitual(server)
   const publishAllShards = PublishAllShards(server)
 
   return function ({ name, secret, quorum, recps }, callback) {
-    if (!callback && typeof callback !== 'function') {
-      throw new Error('provide a callback')
-    }
+    if (!name && !isString(name)) throw new Error('name must be a string')
+    if (!secret && !isString(secret)) throw new Error('secret must be a string')
+    if (!quorum && !isNumber(quorum)) throw new Error('quorum must be a number')
+    if (!recps && !Array.isArray(recps)) throw new Error('recps must be an array')
+    if (!callback && !isFunction(callback)) throw new Error('callback is not a function')
 
-    var validator = prePublishValidation()
-    if (!validator.valid) return callback(valid.errors.join(', '))
+    let feedIds = recps
+      .map(recp => typeof recp === 'string' ? recp : recp.link)
+      .filter(Boolean)
+      .filter(isFeed)
+    let recipients = [...new Set(feedIds)]
+
+    if (recipients.length < 1) return callback(new Error(`data.recps: must be a feedId`))
+    if (recipients.includes(server.id)) return callback(new Error(`data.recps: can't include ${server.id}`))
+    if (quorum === 0) return callback(new Error(`data.quorum: must be greater than 0`))
+    if (recipients.length < quorum) return callback(new Error(`data.quorum: greater than number of recps`))
 
     const numOfShards = recps.length
 
@@ -34,7 +48,7 @@ module.exports = function (server) {
         if (err) return callback(err)
 
         // QUESTION: Can we somehow wrap the shard publication in a single database 'transaction'?
-        // SOLUTION: Have a publishShards (plural) function which validates each with isShard before publishing all
+        // TEMP SOLUTION: Have a publishAllShards (plural) function which validates each with isShard before publishing all
         // RESOLUTION: Extracted reducer into a publishAll function
         publishAllShards({ shards, recps, rootId }, (err, shards) => {
           if (err) callback(err)
@@ -42,49 +56,7 @@ module.exports = function (server) {
         })
       })
     })
-
-    function prePublishValidation () {
-      let recipients = sanitize(recps)
-
-      var errors = []
-      if (recipients.length < 1)
-        errors.push(new Error(`data.recps: must be a feedId`))
-
-      if (recipients.includes(server.id))
-        errors.push(new Error(`data.recps: doesn't need to include self`))
-
-      if (!isNumber(quorum))
-        errors.push(new Error('data.quorum: must be a number'))
-
-      if (quorum === 0)
-        errors.push(new Error(`data.quorum: must be greater than 0`))
-
-      if (recipients.length < quorum)
-        errors.push(new Error(`data.quorum: greater than number of recps`))
-
-      return {
-        valid: errors.length === 0,
-        errors: errors
-      }
-    }
   }
-}
-
-function isNumber (variable) {
-  return Number(variable) !== NaN
-}
-
-function sanitizeRecps (recps) {
-  if (!Array.isArray(recps)) return []
-
-  // this removes the { name: 'thingy', link: '@...' },
-  // instead just returns feedId '@...'
-  let feedIds = recps
-    .map(recp => typeof recp === 'string' ? recp : recp.link)
-    .filter(Boolean)
-    .filter(isFeed)
-
-  return [...new Set(feedIds)]
 }
 
 // getRoot(rootId, (err, root) => {
--- test/share/async/share.test.js
+++ test/share/async/share.test.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const { describe } = require('tape-plus')
 const Server = require('scuttle-testbot')
 
@@ -6,21 +5,72 @@ const Share = require('../../../share/async/share')
 
 describe('share.async.share', context => {
   let server
+  let share
+  let recps, name, secret, quorum
 
   context.beforeEach(c => {
     server = Server()
     share = Share(server)
+    recps = [
+      server.createFeed().id,
+      server.createFeed().id,
+      server.createFeed().id
+    ]
+    name = "Dark Crystal"
+    secret = Math.random().toString(36)
+    quorum = 3
   })
 
   context.afterEach(c => {
     server.close()
   })
 
-  context('with an existing root message', assert => {
-
+  context('takes a specific set of arguments', assert => {
+    assert.equal(2, share.length, 'takes two arguments')
   })
 
-  context('without an existing root message', assert => {
+  context('validates the params', assert => {
+    assert.throws(() => share({ secret, quorum, recps }, () => {}), 'throws an error when name is missing')
+    assert.throws(() => share({ name, quorum, recps }, () => {}), 'throws an error when secret is missing')
+    assert.throws(() => share({ name, secret, recps }, () => {}), 'throws an error when quorum is missing')
+    assert.throws(() => share({ name, secret, quorum }, () => {}), 'throws an error when recps is missing')
+    assert.throws(() => share({ name, secret, quorum, recps }), 'throws an error when callback is missing')
+    assert.throws(() => share({ name, secret, quorum, recps }, 'not a callback'), 'throws an error when callback is not a function')
+  })
 
+  context('validates recps', (assert, next) => {
+    recps = ['thisisnotafeed']
+    share({ name, secret, quorum, recps }, (err, data) => {
+      assert.ok(err, 'raises error')
+      assert.notOk(data, 'data is undefined')
+      assert.equal(err.message, 'data.recps: must be a feedId', 'invalid feedId')
+
+      recps = [server.id]
+      share({ name, secret, quorum, recps }, (err, data) => {
+        assert.ok(err, 'raises error')
+        assert.notOk(data, 'data is undefined')
+        assert.equal(err.message, `data.recps: can't include ${server.id}`, 'raises error when includes self')
+        next()
+      })
+    })
+  })
+
+  context('validates quorum', (assert, next) => {
+    quorum = 0
+    share({ name, secret, quorum, recps }, (err, data) => {
+      assert.ok(err)
+      assert.notOk(data, 'data is undefined')
+      assert.equal(err.message, 'data.quorum: must be greater than 0', 'invalid quorum')
+
+      quorum = recps.length + 1
+
+      share({ name, secret, quorum, recps }, (err, data) => {
+        assert.ok(err)
+        assert.notOk(data, 'data is undefined')
+        assert.equal(err.message, 'data.quorum: greater than number of recps', 'invalid quorum')
+
+        next()
+      })
+    })
   })
 })
