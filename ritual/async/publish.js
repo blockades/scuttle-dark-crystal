461decae5969c80934ff99a1e0f73a070655800c
ameba23 <ameba23@systemli.org>
2018-07-20 15:21:17 +0200

    added tool and tool version to publish ritual

--- ritual/async/publish.js
+++ ritual/async/publish.js
@@ -1,10 +1,13 @@
 const { isRitual, SCHEMA_VERSION } = require('ssb-dark-crystal-schema')
+const tool = 'secrets.js-grempe'
+const toolVersion = require('../../package.json').dependencies[tool]
 
 module.exports = function (server) {
   return function publish (params, callback) {
     var content = Object.assign({}, {
       type: 'dark-crystal/ritual',
       version: SCHEMA_VERSION,
+      tool: tool + toolVersion,
       recps: [server.id]
     }, params)
 
