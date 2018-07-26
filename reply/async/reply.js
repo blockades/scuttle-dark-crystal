618df2d2a931627b746bcd938566b58acf5996d0
ameba23 <ameba23@systemli.org>
2018-07-26 19:44:47 +0200

    add some bits to reply method

--- reply/async/reply.js
+++ reply/async/reply.js
@@ -9,10 +9,34 @@ module.exports = function (server) {
 
   return function request (rootId, inviteId, callback) {
     // Maybe we dont need to take rootId as an argument because
-    // we can get it from the invite itself
+    // we can get it from the invite message itself
 
     // we need to write a query to find the shard message
     // associated with rootId, and unbox the shard part
+
+    const findShard = (root) => {
+      return {
+        query: [{
+          $filter: {
+            value: {
+              content: {
+                type: 'dark-crystal/shard',
+                root
+              }
+            }
+          }
+        }]
+      }
+    }
+    
+    // not sure if we need a pull stream since we are 
+    // expecting to find exactly one message
+    // hmmm... 
+    msg = server.query.read(findShard(rootId))
+    
+    // should really use that module which
+    // returns just the message part
+    theDecryptedShard = server.private.unbox(msg.value.content.shard)
 
     var reply = {
       root: rootId,
