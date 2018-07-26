2a450d6e8071773c6d8d8a69cd6d3d7cfbffcddb
ameba23 <ameba23@systemli.org>
2018-07-26 11:23:38 +0200

    add invites plugin to testbot

--- test/testbot.js
+++ test/testbot.js
@@ -1,7 +1,9 @@
 module.exports = function Testbot (opts) {
   const server = require('scuttle-testbot')
     .use(require('ssb-private'))
+    .use(require('scuttle-invite-db'))
     .use(require('ssb-query'))
+    .use(require('ssb-backlinks'))
     .call(opts)
 
   return server
