e17d9bcd40562b4ef618620e2b8d1740f548563e
ameba23 <ameba23@systemli.org>
2018-07-28 17:50:21 +0200

    refactor and add some stuff to recombine method

--- recombine/async/recombine.js
+++ recombine/async/recombine.js
@@ -7,6 +7,7 @@ const { isInvite, isReply } = require('scuttle-invite-schema')
 const pullRitual = require('../../ritual/pull/mine')
 const { box } = require('ssb-keys')
 const secrets = require('secrets.js-grempe')
+const getContent = require('ssb-msg-content')
 
 module.exports = function (server) {
   const invites = Invites(server)
@@ -14,14 +15,14 @@ module.exports = function (server) {
   return function recombine (rootId, callback) {
     if (!ref.isMsgId(rootId)) return callback(new Error('Invalid rootId'))
 
-    const findReplies = (root) => {
+    const findAssociatedMessages = (type) => {
       return {
         query: [{
           $filter: {
             value: {
               content: {
-                type: 'invite-reply',
-                root
+                type,
+                root: rootId
               }
             }
           }
@@ -30,42 +31,64 @@ module.exports = function (server) {
     }
     
     // get the quorum from the ritual message
-    // not exactly sure how to use this method
-    pull ( pullRitual( need to give the rootId to opts ) )    
+    // TODO: use
+    // pullRitual( need to give the rootId to opts ) )    
+    
+    var encryptedShards = {}
 
     pull(
-      server.query.read(findReplies(rootId)),
-      pull.map((replyMsg) => {
+      server.query.read(findAssociatedMessages('dark-crystal/ritual')),
+      pull.collect((err, rituals) => {
+        // TODO: verify rituals.length === 1
+        var quorum = getContent(rituals[0]).quorum
 
-        // TODO: this unboxed shard validation should be moved
-        // to a separate method so that it can be used independently
-
-        // validate that body is a shard using secrets.js 
-        try {
-          var shardComponents = secrets.extractShareComponents(msg.body)
-        }
-        catch (err) {
-          return callback(err)
-        }
-     
-        // // TODO: need to write query to get shard messages
-        // // verify that the shard is the shard we sent
-        // if (shardForThatAuthor.shard != box(replyMsg.body, [replyMsg.author]))
-        //   return callback(new Error('Recieved shard does not match given shard for shardholder ',replyMsg.author))
-
-
-        return replyMsg.body 
-      }),
-      pull.collect((err, shards) => {
-        if (shards.length < quorum) return callback(new Error('Not enough shards to recombine'))
-        try {
-          secret = secrets.combine(shards) 
-        } 
-        catch (err) {
-          return callback(err)
-        }
-        callback(null,secret)
+        pull(
+          server.query.read(findAssociatedMessages('dark-crystal/shard')),
+          pull.drain((shardMsg) => {
+            encryptedShards[shardMsg.value.author] = getContent(shardMsg).shard
+          }, gotAllShards(encryptedShards))
+        )
       })
     )
+
   }
+}
+
+
+function gotAllShards(encryptedShards) {
+  
+  pull(
+    server.query.read(findAssociatedMessages('invite-reply')),
+    pull.map((replyMsg) => {
+
+      var shard = getContent(replyMsg).body
+
+      // TODO: this unboxed shard validation should be moved
+      // to a separate method so that it can be used independently
+
+      // validate that shard is a shard using secrets.js 
+      try {
+        var shardComponents = secrets.extractShareComponents(shard)
+      }
+      catch (err) {
+        return callback(err)
+      }
+   
+      // verify that the shard is the shard we sent
+      if (encryptedShards[replyMsg.author] != box(shard, [replyMsg.author]))
+         return callback(new Error('Recieved shard does not match given shard for shardholder ',replyMsg.author))
+
+      return shard 
+    }),
+    pull.collect((err, shards) => {
+      if (shards.length < quorum) return callback(new Error('Not enough shards to recombine'))
+      try {
+        secret = secrets.combine(shards) 
+      } 
+      catch (err) {
+        return callback(err)
+      }
+      callback(null,secret)
+    })
+  )
 }
