e28645fac041c41e6087b26879ede0600580cf51
Kieran <kieran.a.gibb@gmail.com>
2018-08-02 11:11:43 +0100

    insert missing $ and correct _opts

--- recover/pull/requests.js
+++ recover/pull/requests.js
@@ -9,7 +9,7 @@ module.exports = function (server) {
       query: [{
         $filter: {
           value: {
-            timestamp: { gt: 0 },
+            timestamp: { $gt: 0 },
             author: server.id,
             content: {
               type: 'invite',
@@ -21,7 +21,7 @@ module.exports = function (server) {
     }, opts)
 
     return pull(
-      next(server.query.read, opts),
+      next(server.query.read, _opts),
       pull.filter(isInvite)
     )
   }
