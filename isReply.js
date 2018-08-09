942e6d9ea39ab066fbff025a9e7f6bb19628e9ac
mixmix <whimful@gmail.com>
2018-08-09 23:15:10 +1200

    improve the definition of isReply

--- isReply.js
+++ isReply.js
@@ -1,1 +1,7 @@
-module.exports = require('scuttle-invite/isReply')
+const isReply = require('scuttle-invite/isReply')
+const getContent = require('ssb-msg-content')
+
+// TODO write an actual schema for replies in the dc context
+module.exports = function (msg) {
+  return isReply(msg) && getContent(msg).body
+}
