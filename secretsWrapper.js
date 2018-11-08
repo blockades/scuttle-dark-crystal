c6c991126fafbcf91611f7548d3ae81467e2536e
ameba23 <ameba23@systemli.org>
2018-11-08 17:13:20 +0100

    fix base64 encoding

--- secretsWrapper.js
+++ secretsWrapper.js
@@ -6,11 +6,23 @@ module.exports = {
     const hexSecret = secrets.str2hex(secret)
     const hashOfSecret = crypto.createHash('sha1').update(secret, 'binary').digest('hex')
     const shardsHex = secrets.share(hexSecret + hashOfSecret, numOfShards, quorum)
-    return shardsHex.map(shard => Buffer.from(shard, 'utf8').toString('base64'))
+  
+    const shardsBase64 = shardsHex.map(shard => {
+      const shardData = shard.slice(3)
+      const shardDataBase64 = Buffer.from(shardData, 'hex').toString('base64')
+      return shard.slice(0,3) + shardDataBase64
+    })
+    console.log('IN', shardsBase64)
+    console.log(shardsHex[0].length, shardsBase64[0].length)
+    return shardsBase64
   },
 
   combine: function (shardsBase64) {
-    const shards = shardsBase64.map(shard => Buffer.from(shard, 'base64').toString('utf8'))
+    const shards = shardsBase64.map(shard => {
+      const shardData = shard.slice(3)
+      const shardDataHex = Buffer.from(shardData, 'base64').toString('hex')
+      return shard.slice(0,3) + shardDataHex
+    })
     // this could probably be improved by checking the hash before converting to hex
     const hex = secrets.combine(shards)
     const hashOfSecret = hex.slice(-40)
