a4fefc4c08489db32ca255dce29d04585d716322
ameba23 <ameba23@systemli.org>
2018-11-08 13:59:29 +0100

    encode shards to base64 to reduce their size, update validator function

--- isReply.js
+++ isReply.js
@@ -1,17 +1,8 @@
 const isReply = require('scuttle-invite/isReply')
 const getContent = require('ssb-msg-content')
-const secrets = require('secrets.js-grempe')
+const { validateShard } = require('./secretsWrapper')
 
 module.exports = function (msg) {
-  return isReply(msg) && validateShard(msg)
-}
-
-function validateShard (possibleReply) {
-  const shard = getContent(possibleReply).body
-  try {
-    secrets.extractShareComponents(shard)
-  } catch (err) {
-    return false
-  }
-  return true
+  const msgContent = getContent(msg).body
+  return isReply(msg) && validateShard(msgContent)
 }
--- secretsWrapper.js
+++ secretsWrapper.js
@@ -5,10 +5,12 @@ module.exports = {
   share: function (secret, numOfShards, quorum) {
     const hexSecret = secrets.str2hex(secret)
     const hashOfSecret = crypto.createHash('sha1').update(secret, 'binary').digest('hex')
-    return secrets.share(hexSecret + hashOfSecret, numOfShards, quorum)
+    const shardsHex = secrets.share(hexSecret + hashOfSecret, numOfShards, quorum)
+    return shardsHex.map(shard => Buffer.from(shard, 'utf8').toString('base64'))
   },
 
-  combine: function (shards) {
+  combine: function (shardsBase64) {
+    const shards = shardsBase64.map(shard => Buffer.from(shard, 'base64').toString('utf8'))
     // this could probably be improved by checking the hash before converting to hex
     const hex = secrets.combine(shards)
     const hashOfSecret = hex.slice(-40)
@@ -18,5 +20,14 @@ module.exports = {
     } else {
       return secret
     }
+  },
+  validateShard: function (shardBase64) {
+    const shard = Buffer.from(shardBase64, 'base64').toString('utf8')
+    try {
+      secrets.extractShareComponents(shard)
+    } catch (err) {
+      return false
+    }
+    return true
   }
 }
