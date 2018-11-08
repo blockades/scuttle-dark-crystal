5dbd36e651df261fb566c8326d512c8ed43d65e2
ameba23 <ameba23@systemli.org>
2018-11-08 17:24:05 +0100

    update validator for shards and make separate pack and unpack functions

--- secretsWrapper.js
+++ secretsWrapper.js
@@ -1,28 +1,30 @@
 const secrets = require('secrets.js-grempe')
 const crypto = require('crypto')
 
+const packShard = shard => {
+  const shardData = shard.slice(3)
+  const shardDataBase64 = Buffer.from(shardData, 'hex').toString('base64')
+  return shard.slice(0, 3) + shardDataBase64
+}
+
+const unpackShard = shard => {
+  const shardData = shard.slice(3)
+  const shardDataHex = Buffer.from(shardData, 'base64').toString('hex')
+  return shard.slice(0, 3) + shardDataHex
+}
+
 module.exports = {
   share: function (secret, numOfShards, quorum) {
     const hexSecret = secrets.str2hex(secret)
     const hashOfSecret = crypto.createHash('sha1').update(secret, 'binary').digest('hex')
     const shardsHex = secrets.share(hexSecret + hashOfSecret, numOfShards, quorum)
-  
-    const shardsBase64 = shardsHex.map(shard => {
-      const shardData = shard.slice(3)
-      const shardDataBase64 = Buffer.from(shardData, 'hex').toString('base64')
-      return shard.slice(0,3) + shardDataBase64
-    })
-    console.log('IN', shardsBase64)
-    console.log(shardsHex[0].length, shardsBase64[0].length)
+
+    const shardsBase64 = shardsHex.map(packShard)
     return shardsBase64
   },
 
   combine: function (shardsBase64) {
-    const shards = shardsBase64.map(shard => {
-      const shardData = shard.slice(3)
-      const shardDataHex = Buffer.from(shardData, 'base64').toString('hex')
-      return shard.slice(0,3) + shardDataHex
-    })
+    const shards = shardsBase64.map(unpackShard)
     // this could probably be improved by checking the hash before converting to hex
     const hex = secrets.combine(shards)
     const hashOfSecret = hex.slice(-40)
@@ -34,7 +36,7 @@ module.exports = {
     }
   },
   validateShard: function (shardBase64) {
-    const shard = Buffer.from(shardBase64, 'base64').toString('utf8')
+    const shard = unpackShard(shardBase64)
     try {
       secrets.extractShareComponents(shard)
     } catch (err) {
