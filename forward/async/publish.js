b765cce35b7a7d024148e7456ac7f65e6fe8561a
ameba23 <ameba23@systemli.org>
2018-11-14 22:04:44 +0100

    improve forward publish method

--- forward/async/publish.js
+++ forward/async/publish.js
@@ -1,20 +1,52 @@
 const { isForward, SCHEMA_VERSION } = require('ssb-dark-crystal-schema')
+const pull = require('pull-stream')
+const PullShardsByRoot = require('../../shard/pull/by-root')
+const ref = require('ssb-ref')
 
 module.exports = function (server) {
-  return function publish (root, shard, recp, callback) {
-    var content = {
-      type: 'dark-crystal/forward',
-      version: SCHEMA_VERSION,
-      root,
-      shard,
-      recps: [recp, server.id]
-    }
+  const pullShardsByRoot = PullShardsByRoot(server)
+  return function publish (root, recp, callback) {
+    if (!ref.isMsgId(root)) return callback(new Error('Invalid rootId'))
+    pull(
+      pullShardsByRoot(root),
+      pull.collect((err, shards) => {
+        if (err) return callback(err)
 
-    if (isForward(content)) {
-      server.private.publish(content, [server.id], (err, forward) => {
-        if (err) callback(err)
-        else server.private.unbox(forward, callback)
+        if (shards.length < 1) {
+          let error = new Error('There are no shards associated with rootId ', root)
+          return callback(error)
+        }
+
+        if (shards.length > 1) {
+          let error = new Error('You have more than one shard for this secret, not yet supported')
+          return callback(error)
+        }
+
+        const {
+          value: {
+            content: { shard }
+          }
+        } = shards[0]
+
+        server.private.unbox(shard, (err, theDecryptedShard) => {
+          if (err) return callback(err)
+
+          var content = {
+            type: 'dark-crystal/forward',
+            version: SCHEMA_VERSION,
+            root,
+            shard: theDecryptedShard,
+            recps: [recp, server.id]
+          }
+
+          if (isForward(content)) {
+            server.private.publish(content, [server.id], (err, forward) => {
+              if (err) callback(err)
+              else server.private.unbox(forward, callback)
+            })
+          } else callback(isForward.errors)
+        })
       })
-    } else callback(isForward.errors)
+    )
   }
 }
--- test/forward/async/publish.test.js
+++ test/forward/async/publish.test.js
@@ -1,22 +1,33 @@
 const { describe } = require('tape-plus')
 const Server = require('../../testbot')
+const { SCHEMA_VERSION } = require('ssb-dark-crystal-schema')
+const { box } = require('ssb-keys')
 
 const Publish = require('../../../forward/async/publish')
 
 describe('forward.async.publish', context => {
   let server
   let publish
-  let alice
+  let alice, bob
   let root
-  let shard
+  let shard, bobShard
 
   context.beforeEach(c => {
     server = Server()
     publish = Publish(server)
 
     alice = server.createFeed()
+    bob = server.createFeed()
     root = '%g1gbRKarJT4au9De2r4aJ+MghFSAyQzjfVnnxtJNBBw=.sha256'
     shard = 'imagine this is a shard'
+
+    bobShard = {
+      type: 'dark-crystal/shard',
+      version: SCHEMA_VERSION,
+      root,
+      shard: box(shard, [ server.id ]),
+      recps: [server.id, bob.id]
+    }
   })
 
   context.afterEach(c => {
@@ -24,17 +35,20 @@ describe('forward.async.publish', context => {
   })
 
   context('publishes a message when valid', (assert, next) => {
-    publish(root, shard, alice.id, (err, forward) => {
-      assert.notOk(err, 'null errors')
-      assert.ok(forward, 'valid forward object')
-      assert.equal(shard, forward.value.content.shard, 'shard is inserted')
-      next()
+    bob.publish(bobShard, (err, bobReply) => {
+      if (err) console.error(err)
+      publish(root, alice.id, (err, forward) => {
+        assert.notOk(err, 'null errors')
+        assert.ok(forward, 'valid forward object')
+        assert.equal(shard, forward.value.content.shard, 'shard is inserted')
+        next()
+      })
     })
   })
 
   context('fails to publish when invalid', (assert, next) => {
     root = 'this is not a root'
-    publish(root, shard, alice.id, (errs, forward) => {
+    publish(root, alice.id, (errs, forward) => {
       assert.ok(errs, 'has errors')
       assert.notOk(forward, 'forward is null')
       next()
