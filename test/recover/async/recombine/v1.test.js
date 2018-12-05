600485fe0c565620711021a0de2eaecaab46b9da
ameba23 <ameba23@systemli.org>
2018-12-05 19:38:15 +0100

    extra test for recombine

--- test/recover/async/recombine/v1.test.js
+++ test/recover/async/recombine/v1.test.js
@@ -81,7 +81,7 @@ describe('recombine.async.recombine', context => {
 
             recombine(rootId, (err, returnedSecret) => {
               assert.notOk(err, 'error is null')
-              assert.equal(secret, returnedSecret, 'returns the correct secret')
+              assert.equal(secret, returnedSecret.secret, 'returns the correct secret')
               next()
             })
           })
@@ -237,6 +237,58 @@ describe('recombine.async.recombine', context => {
       assert.ok(err, 'has error')
       assert.notOk(returnedSecret, 'Does not return a secret')
       next()
+    })
+  })
+
+  context('Returns the nickname if one is given', (assert, next) => {
+    var replies = {}
+    const nickname = 'Give this key to your nearest and dearest'
+    share({ name, secret, quorum, nickname, recps: shardHolders }, (err, data) => {
+      if (err) console.error(err)
+      var rootId = data.root.key
+
+      request(rootId, (err, inviteMsgs) => {
+        if (err) console.error(err)
+
+        inviteMsgs.forEach((inviteMsg) => {
+          var inviteMsgContent = getContent(inviteMsg)
+          var shardHolder = inviteMsgContent.recps.filter(recp => recp !== server.id)[0]
+          var shardMsgs = data.shards.map(s => (getContent(s)))
+          var shard = shardMsgs.filter(s => (s.recps.find(r => (r === shardHolder))))[0].shard
+          // We need to recreate replies from alice, and bob:
+          const reply = {
+            type: 'invite-reply',
+            root: rootId,
+            branch: inviteMsg.key,
+            accept: true,
+            version: '1',
+            recps: inviteMsgContent.recps
+          }
+          if (shardHolder === alice.id) {
+            reply.body = unbox(shard, alice.keys)
+            replies['alice'] = reply
+          }
+          if (shardHolder === bob.id) {
+            reply.body = unbox(shard, bob.keys)
+            replies['bob'] = reply
+          }
+        })
+
+        alice.publish(replies.alice, (err, aliceReply) => {
+          if (err) console.error(err)
+
+          bob.publish(replies.bob, (err, bobReply) => {
+            if (err) console.error(err)
+
+            recombine(rootId, (err, returnedSecret) => {
+              assert.notOk(err, 'error is null')
+              assert.equal(secret, returnedSecret.secret, 'returns the correct secret')
+              assert.equal(nickname, returnedSecret.nickname, 'returns the correct nickname')
+              next()
+            })
+          })
+        })
+      })
     })
   })
 })
