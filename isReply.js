fee1d8456868a17f58aac7a5d317fa003c6f64fb
ameba23 <ameba23@systemli.org>
2018-12-06 12:40:55 +0100

    modify isReply to check the version in the reply message

--- isReply.js
+++ isReply.js
@@ -2,13 +2,13 @@ const _isReply = require('scuttle-invite/isReply')
 const getContent = require('ssb-msg-content')
 const { validateShard } = require('./lib/secrets-wrapper')
 
-module.exports = function isReply (msg, version) {
+module.exports = function isReply (msg) {
   const msgContent = getContent(msg)
   const shard = msgContent.body
-
+  let shardVersion = msgContent.shardVersion || '1.0.0'
   const errors = []
   if (!_isReply(msg)) errors.push(_isReply.errors)
-  if (!validateShard(shard, version)) errors.push(new Error('invalid shard'))
+  if (!validateShard(shard, shardVersion)) errors.push(new Error('invalid shard'))
 
   if (!errors.length) return true
 
