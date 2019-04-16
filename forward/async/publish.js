0f45ea343f54348a209b2833731cd837a6bcedbd
ameba23 <ameba23@systemli.org>
2019-04-16 12:23:44 +0200

    forward publish method takes an object, which may contain a requestId

--- forward/async/publish.js
+++ forward/async/publish.js
@@ -4,12 +4,10 @@ const get = require('lodash.get')
 const PullShardsByRoot = require('../../shard/pull/by-root')
 const buildForward = require('./build')
 const publish = require('../../lib/publish-msg')
-const PullForwardRequestsSecretOwner = require('../../forward-request/pull/by-secret-owner')
 
 module.exports = function (server) {
   const pullShardsByRoot = PullShardsByRoot(server)
-  const pullForwardRequestsSecretOwner = PullForwardRequestsSecretOwner(server)
-  return function publishForward (root, recp, callback) {
+  return function publishForward ({ root, recp, requestId }, callback) {
     if (!ref.isMsgId(root)) return callback(new Error('Invalid rootId'))
     pull(
       pullShardsByRoot(root),
@@ -27,26 +25,15 @@ module.exports = function (server) {
         if (get(shards[0], 'value.author') === recp) {
           return callback(new Error('You may not forward a shard to its author. Use reply instead.'))
         }
-        pull(
-          pullForwardRequestsSecretOwner(get(shards[0], 'value.author')),
-          pull.collect((err, forwardRequests) => {
-            var requestId = null
-            if (err) return callback(err)
-            if (forwardRequests.length > 0) {
-              requestId = forwardRequests[0]
-            }
 
-            const shareVersion = get(shards[0], 'value.content.version')
-            const shardId = get(shards[0], 'key')
-            const shard = get(shards[0], 'value.content.shard')
+        const shareVersion = get(shards[0], 'value.content.version')
+        const shardId = get(shards[0], 'key')
+        const shard = get(shards[0], 'value.content.shard')
 
-            buildForward(server)({ root, shard, shardId, requestId, shareVersion, recp }, (err, content) => {
-              if (err) return callback(err)
-
-              publish(server)(content, callback)
-            })
-          })
-        )
+        buildForward(server)({ root, shard, shardId, requestId, shareVersion, recp }, (err, content) => {
+          if (err) return callback(err)
+          publish(server)(content, callback)
+        })
       })
     )
   }
